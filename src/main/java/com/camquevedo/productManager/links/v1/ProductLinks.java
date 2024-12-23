eddb235981181aa02758ee64375cfa34e0825fc6
Camilo Quevedo <camquevedo@hotmail.com>
2024-12-23 11:28:16 -0500

    add product link

--- src/main/java/com/camquevedo/productManager/links/v1/ProductLinks.java
+++ src/main/java/com/camquevedo/productManager/links/v1/ProductLinks.java
@@ -1,4 +1,9 @@
 package com.camquevedo.productManager.links.v1;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ProductLinks {
+
+    public static final String PRODUCTS_CRUD = "/api/products";
 }
